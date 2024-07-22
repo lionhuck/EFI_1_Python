555b5ae2df75a88a0c6ca236580d8a5d0901a353
Leon Federico Huck <leonhuck007@gmail.com>
2024-07-22 11:32:51 -0300

    Pude cargar la base de datos correctamente y verla en localHost. Hay que mejorar la base de datos en cuanto a su funcionalidad y sus respectivas relaciones

--- celulares.py
+++ celulares.py
@@ -7,28 +7,26 @@ class Equipo(db.Model):
     marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
     costo = db.Column(db.Integer, nullable=False)
 
-    modelo = db.relationship('Modelo', backref=db.backref('equipo', lazy=True))
-    marca = db.relationship('Marca', backref=db.backref('equipo', lazy=True))       
-    
+    modelo = db.relationship('Modelo', backref=db.backref('equipos', lazy=True))
+    marca = db.relationship('Marca', backref=db.backref('equipos', lazy=True))       
 
 class Modelo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
     fabricante_id = db.Column(db.Integer, db.ForeignKey('fabricante.id'), nullable=False)
     
-    fabricante = db.relationship('Fabricante', backref=db.backref('modelo'),lazy=True )
+    fabricante = db.relationship('Fabricante', backref=db.backref('modelos', lazy=True))
 
 class Marca(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    categoria = db.Column(db.String(30), nullable=False)
+    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'), nullable=False)
 
- 
+    tipo = db.relationship('Tipo', backref=db.backref('marcas', lazy=True))
 class Fabricante(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
     pais_origen = db.Column(db.String(30), nullable=False)
-
 
 class Caracteristica(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,10 +36,9 @@ class Caracteristica(db.Model):
 class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cantidad_disponible = db.Column(db.Integer, nullable=False)
-    ubicacionAlmacen_id = db.Column(db.Integer, db.ForeignKey('ubicacionAlmacen.id'), nullable=False)
+    ubicacionAlmacen_id = db.Column(db.Integer, db.ForeignKey('ubicacion_almacen.id'), nullable=False)
 
-    ubicacion = db.relationship('UbicacionAlmacen', backref=db.backref('Stock'),lazy=True )
-
+    ubicacion = db.relationship('UbicacionAlmacen', backref=db.backref('stocks', lazy=True))
 
 class UbicacionAlmacen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,13 +50,17 @@ class Persona(db.Model):
     apellido = db.Column(db.String(30), nullable=False)
     mail = db.Column(db.String(50), nullable=False)
 
+class Tipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+
 class Proveedor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo .id'), nullable=False)
+    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'), nullable=False)
     cantidad_disponible = db.Column(db.String(30), nullable=False)
     ubicacion_almacen = db.Column(db.String(30), nullable=False)    
 
 class Accesorio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    compativilidad = db.Column(db.String(30), nullable=False)
+    compatibilidad = db.Column(db.String(30), nullable=False)
