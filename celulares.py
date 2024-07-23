bd294244688ae6e422c4e905dc6ce3e9834f2210
Leon Federico Huck <leonhuck007@gmail.com>
2024-07-22 21:29:41 -0300

    Ultimas modificaciones

--- celulares.py
+++ celulares.py
@@ -4,30 +4,31 @@ class Equipo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
     modelo_id = db.Column(db.Integer, db.ForeignKey('modelo.id'), nullable=False)    
-    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
     costo = db.Column(db.Integer, nullable=False)
 
     modelo = db.relationship('Modelo', backref=db.backref('equipos', lazy=True))
-    marca = db.relationship('Marca', backref=db.backref('equipos', lazy=True))       
+    categoria = db.relationship('Categoria', backref=db.backref('equipos', lazy=True))       
 
 class Modelo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
     fabricante_id = db.Column(db.Integer, db.ForeignKey('fabricante.id'), nullable=False)
+    caracteristica_id = db.Column(db.Integer, db.ForeignKey('caracteristica.id'), nullable=False)
     
     fabricante = db.relationship('Fabricante', backref=db.backref('modelos', lazy=True))
+    caracteristica = db.relationship('Caracteristica', backref=db.backref('modelos', lazy=True))
 
-class Marca(db.Model):
+class Categoria(db.Model):   #Es donde en el texto dice Marca(me parecio mas pertinente ponerle categoria y no marca)
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'), nullable=False)
 
-    tipo = db.relationship('Tipo', backref=db.backref('marcas', lazy=True))
 class Fabricante(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    pais_origen = db.Column(db.String(30), nullable=False)
+    pais_id = db.Column(db.Integer, db.ForeignKey('pais.id'), nullable=False)
 
+    pais = db.relationship('Pais', backref=db.backref('fabricantes',lazy=True))
 class Caracteristica(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
@@ -36,11 +37,12 @@ class Caracteristica(db.Model):
 class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cantidad_disponible = db.Column(db.Integer, nullable=False)
-    ubicacionAlmacen_id = db.Column(db.Integer, db.ForeignKey('ubicacion_almacen.id'), nullable=False)
+    equipo_id = db.Column(db.Integer, db.ForeignKey('equipo.id'), nullable=False)
+    almacen_id = db.Column(db.Integer, db.ForeignKey('almacen.id'), nullable=False)
 
-    ubicacion = db.relationship('UbicacionAlmacen', backref=db.backref('stocks', lazy=True))
-
-class UbicacionAlmacen(db.Model):
+    almacen = db.relationship('Almacen', backref=db.backref('stocks', lazy=True))
+    equipo = db.relationship('Equipo', backref=db.backref('stocks', lazy=True))
+class Almacen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
 
@@ -63,4 +65,17 @@ class Proveedor(db.Model):
 class Accesorio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    compatibilidad = db.Column(db.String(30), nullable=False)
+
+
+class AccesorioModelo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    accesorio_id = db.Column(db.Integer, db.ForeignKey('accesorio.id'), nullable=False)
+    modelo_id = db.Column(db.Integer, db.ForeignKey('modelo.id'), nullable=False)
+
+    accesorio = db.relationship('Accesorio', backref=db.backref('accesorio_modelos', lazy=True))
+    modelo = db.relationship('Modelo', backref=db.backref('accesorio_modelos', lazy=True))
+
+
+class Pais(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
